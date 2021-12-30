4f8cdaf5fecf5d01ec2eedaea33f99a0c0dc6538
EricGartner47 <egartner47@gmail.com>
2021-12-29 19:04:43 -0500

    partially finished

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
+from app.models import Employee
 from wtforms.fields import (
-    StringField, SubmitField, PasswordField
+    StringField, SubmitField, PasswordField, SelectField
 )
 from wtforms.validators import DataRequired
 
@@ -8,3 +9,7 @@ class LoginForm(FlaskForm):
     employee_number=StringField('employee_number', validators=[DataRequired()])
     password=PasswordField('Password', validators=[DataRequired()])
     submit=SubmitField('Login')
+
+# class AssignEmployee(FlaskForm):
+#     employee_number=SelectField('Employee', choices=[])
+#     table_number=
